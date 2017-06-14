072ea1febb9472453e7f7989f8af890e144778e0
lindseypeng <lindsey.peng@mail.utoronto.ca>
2017-06-14 16:33:16 -0400

    times series images
    1.load a series of images from a folder with the same type of naming conventio
    2.find the minprojection of recontructed hologram for each of the image
    3.save the minproj of each time series image into a series of min proj pictures. ready to be send to segmentation and link

--- convolution.py
+++ convolution.py
@@ -1,162 +1,130 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 25 11:45:54 2017
 
-@author: alinsi
-"""
-
-from mpl_toolkits.mplot3d import Axes3D
+  
 import numpy as np
 import cv2
 import math
-#import holopy as hp
-import imutils
-#import tifffile 
 
 
-import matplotlib.pyplot as plt
-
-from skimage.filters import (threshold_sauvola)
-from skimage.morphology import binary_opening
-from skimage.util import invert
-
-#load image
-image_path="/home/alinsi/Desktop/inlinehologram/may11/further/pnc7010x.bmp"
-
-h = cv2.imread(image_path,0)
 
 
-h1 = np.array(h).astype(float)
 
-(Nx,Ny)= h1.shape[:2]
+##from skimage.morphology import binary_closing
+
+
+#load image series from a directory using for loop
+width=1280
+height=1024
+numbers=20#number of time seried images 
+(Nx,Ny) = (width,height)
 minN = min(Nx,Ny)
-h1 = h1[:minN,:minN]
+stack=np.float32(np.zeros((height,width)))
 
+rangenum=range(numbers)##0,1,2,3,4,..
+##replacing the numbers in  the right foramt
 
-(Nx2,Ny2) = h1.shape[:2]
-
-lambda0 = 0.000488
-delx=5.32/1024
-dely=6.66/1280
-i=complex(0,1)
-pi=math.pi
-maxd=90#looping of distance in mm from object to CCD , maximum is 22cm, 220mm,minmum is 60mm6
-mind=30
-steps=5#distanced looping step
-
-xmax=Nx2*delx/2
-ymax=Ny2*dely/2
-
-nx = np.arange (-Nx2/2,Nx2/2,1)
-ny = np.arange (-Ny2/2,Ny2/2,1)
-
-X=nx*delx
-Y=ny*dely
-
-[XX,YY]=np.meshgrid(X,Y)
-
-
-Gy = 10
-k=2*pi/lambda0
-
-imageslices=(maxd-mind)/steps
-minproj=np.zeros([minN,minN,imageslices])
-
-metricarray=[]
-dp = np.arange(mind, maxd, steps)
-H=np.fft.fft2(h1)
-
-indd=np.arange(1,minN,1)
-
-
-threeD=np.empty((imageslices,minN,minN))
-for d in dp:
-    ind=(d-mind)/steps
-    num = np.exp(-i*2*pi/lambda0*np.sqrt((d**2+XX**2+YY**2)))
-    den = np.sqrt(d**2+XX**2+YY**2)
-    g=i/lambda0*num/den#g is the impulse response of propagation
-    G=np.fft.fft2(g)
-    Rec_image=np.fft.fftshift(np.fft.ifft2(G*H))
-    amp_rec_image = np.abs(Rec_image)
-    threeD[ind,:,:]=amp_rec_image
- 
-    if ind < 1:
-        maxproj = amp_rec_image
-        
-##finding Minimum projection###
+placeholder="/home/alinsi/Desktop/june14beadsonglass7cm/Pos0/img_0000000{}_Default_000.tif"
+for f in rangenum:##looping through every single image##
     
-    for p in indd:
-       for j in indd:
-            if maxproj[p][j] > amp_rec_image[p][j]:
-                maxproj[p][j]=amp_rec_image[p][j]
+   
+    f2="{:02d}".format(f)
+    image_path=placeholder.format((f2))
+    h = np.float32(cv2.imread(image_path,0))
 
-#hp.show(threeD)
+    stack +=h 
 
-#tifffile.imsave("/home/alinsi/Desktop/detection/stacks")
-######################FINDING INFOCUSE XY PLANE#################################
-window_size = 25
-#maxproj = np.array(maxproj*255,dtype=np.uint8)
-maxproj*=255.0/maxproj.max()
+averagestack=stack/numbers
+newrawstack=np.float32(np.zeros((numbers,height,width)))
+minprojstack = np.empty((numbers,minN,minN))
 
-#binary_global = maxproj > threshold_otsu(maxproj)
-#thresh_niblack = threshold_niblack(maxproj, window_size=window_size, k=0.8)
-thresh_sauvola = threshold_sauvola(maxproj, window_size=window_size, k=0.5, r=128)
+for f in rangenum:##looping through every single image##
 
-#binary_niblack = maxproj > thresh_niblack
-binary_sauvola = maxproj > thresh_sauvola
+    f2="{:02d}".format(f)
+    image_path=placeholder.format((f2))
+    h = np.float32(cv2.imread(image_path,0))
+    newraw=h/averagestack
+    newrawstack[f,:,:] += newraw
 
-opening=binary_opening(invert(binary_sauvola),np.ones((3,3),np.uint8))
-opening=opening.astype(np.uint8)
-maxproj=maxproj.astype(np.uint8)
-color=cv2.cvtColor(maxproj,cv2.COLOR_GRAY2RGB)
+    h1 = np.array(newraw).astype(float)
+
+    (Nx,Ny)= h1.shape[:2]
+#h=cv2.imread('/home/alinsi/Desktop/june14beadsonglass7cm/Pos0/normalized.jpg',0)       
 
 
-cnts=cv2.findContours(opening.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-cnts=cnts[0] if imutils.is_cv2() else cnts[1]
-
-cXs=[]
-cYs=[]
-#loop over the contours
-for i,c in enumerate (cnts):
-     M = cv2.moments(c)
-     cX = int(M["m10"] / M["m00"])
-     cY = int(M["m01"] / M["m00"])
-     (x,y,w,h) = cv2.boundingRect(c)
-     cXs.append(cX)
-     cYs.append(cY)
-     #cv2.drawContours(color, [c], -1, (0, 255, 0), 1)
-     #cv2.circle(color, (cX, cY), 1, (255, 255, 255), -1)
-     #cv2.rectangle(color,(x,y),(x+w,y+h),(0,255,255),1)
-     #cv2.putText(color, "center", (cX - 20, cY - 20),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-     
-     for d in dp:
+    #h1 = np.array(h).astype(float)
+    h1 = h1[:minN,:minN]
+    
+    
+    (Nx2,Ny2) = h1.shape[:2]
+    
+    lambda0 = 0.000488
+    delx=5.32/1024
+    dely=6.66/1280
+    i=complex(0,1)
+    pi=math.pi
+    maxd=70#looping of distance in mm from object to CCD , maximum is 22cm, 220mm,minmum is 60mm6
+    mind=40
+    steps=5#distanced looping step
+    
+    xmax=Nx2*delx/2
+    ymax=Ny2*dely/2
+    
+    nx = np.arange (-Nx2/2,Nx2/2,1)
+    ny = np.arange (-Ny2/2,Ny2/2,1)
+    
+    X=nx*delx
+    Y=ny*dely
+    
+    [XX,YY]=np.meshgrid(X,Y)
+    
+    
+    Gy = 10
+    k=2*pi/lambda0
+    
+    imageslices=(maxd-mind)/steps
+    minproj=np.zeros([minN,minN,imageslices])
+    
+    metricarray=[]
+    dp = np.arange(mind, maxd, steps)
+    H=np.fft.fft2(h1)
+    
+    indd=np.arange(1,minN,1)
+    
+    
+    threeD=np.empty((imageslices,minN,minN))##initializing for stack
+    
+    
+    #threeDF=np.empty((imageslices,minN,minN))
+    for d in dp:
         ind=(d-mind)/steps
-        particle = threeD[ind,y:y+h,x:x+w]
-        Metric=np.linalg.norm(particle)
-        print Metric
-        metricarray=np.append(metricarray,[Metric])
-        
-cv2.imshow("labeled",color)
-metricarray2=metricarray.reshape(len(cnts),len(dp))
+        num = np.exp(-i*2*pi/lambda0*np.sqrt((d**2+XX**2+YY**2)))
+        den = np.sqrt(d**2+XX**2+YY**2)
+        g=i/lambda0*num/den#g is the impulse response of propagation
+        G=np.fft.fft2(g)
+        intermediate=np.real(G*H)
+        Rec_image=np.fft.fftshift(np.fft.ifft2(G*H))
+        amp_rec_image = np.abs(Rec_image)
+        threeD[ind,:,:]=amp_rec_image.astype(np.float32)
+        #threeDF[ind,:,:]=intermediate.astype(np.float32)
+    #        
+        if ind < 1:
+            maxproj = amp_rec_image
+    #            
+    #    ##finding Minimum projection###
+    #        
+        for p in indd:
+           for j in indd:
+                if maxproj[p][j] > amp_rec_image[p][j]:
+                    maxproj[p][j]=amp_rec_image[p][j]
+    #    
+    #    
+    #hp.show(threeD.transpose())
+#    
+#    #tifffile.imsave("/home/alinsi/Desktop/detection/stacks")
+#    ######################FINDING INFOCUSE XY PLANE#################################
 
-#the minimum of rows is axis=1, col is axis=0
+   # maxproj2 = np.array(maxproj*255,dtype=np.uint8)
+   # maxproj2=maxproj2*255.0/maxproj2.max()
+#    
+    minprojstack[f,:,:]=maxproj
 
-minimumvalue=metricarray2.min(axis=1)
-#this gives the minimum value of each xy object
-
-minimumdp=np.argmin(metricarray2,axis=1)
-ranges = np.arange(0, len(minimumdp), 1)
-
-for i in ranges:
-    print ("The focused distance for particle #{} is {} mm ".format(i+1,minimumdp[i]))
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection ='3d')
-
-ax.scatter(cXs,cYs,minimumdp, c='r', marker='o')
-
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z lebel')
-
-plt.show()
+#tifffile.imsave("/home/alinsi/Desktop/normalizedstacksminproj",minprojstack)
