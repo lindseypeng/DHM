f341cef4947b2eaa5c772ac14b1c8176fcc6e52a
lindseypeng <lindsey.peng@mail.utoronto.ca>
2018-10-25 15:35:20 -0400

    Update convolutiononly.py

--- convolutiononly.py
+++ convolutiononly.py
@@ -1,78 +1,67 @@
 import numpy as np
 import cv2
 import math
-#import holopy as hp
-#import imutils
 import tifffile 
-
 import timeit
 import matplotlib.pyplot as plt
-
-#from skimage.filters import threshold_sauvola
 from skimage.morphology import binary_opening
 from skimage.util import invert
 
-
-
-start_time=timeit.default_timer()
 #load image
 image_path="C:/Users/Yip Group/Desktop/june261/secondconcentration/cellulose/Pos0/normalized18.tif"
-
-
-h = cv2.imread(image_path,0)
-
-
-h1 = np.array(h).astype(float)
-
-(Nx,Ny)= h1.shape[:2]
-minN = min(Nx,Ny)
-h1 = h1[:minN,:minN]
-
-
+###################################################Set Up########################################################
+##Input hologram##
+h1 = cv2.imread(image_path,0)
+##Extract row and column pixel sizes##
 (Nx2,Ny2) = h1.shape[:2]
-
-lambda0 = 0.000488
-delx=5.32/1024
-dely=6.66/1280
-i=complex(0,1)
-pi=math.pi
-maxd=90#looping of distance in mm from object to CCD , maximum is 22cm, 220mm,minmum is 60mm6
-mind=40
-steps=2#distanced looping step
-
-xmax=Nx2*delx/2
+##Input Variable##
+lambda0 = 0.000488  ##wavelength of laser
+delx=5.32/1024      ##row pitch size    
+dely=6.66/1280      ##column pitch size     
+i=complex(0,1)      ##complex number
+pi=math.pi          
+maxd=90             ##max reconstruction distance in mm
+mind=80             ##min reconstruction distance in mm
+steps=1             ##step sizes of reconstruction in mm
+imageslices=(maxd-mind)/steps ##number of reconstruction images
+minproj=np.zeros([minN,minN,imageslices]) ##create placeholder for an stack of reconstruction images 
+metricarray=[]
+dp = np.arange(mind, maxd, steps)         #list containing reconstruction steps to be iterated
+indd=np.arange(1,minN,1)    ##index to step through
+##Set up Spatial coordinates of impulse function that matches with hologram aperture size##
+xmax=Nx2*delx/2     
 ymax=Ny2*dely/2
-
 nx = np.arange (-Nx2/2,Nx2/2,1)
 ny = np.arange (-Ny2/2,Ny2/2,1)
-
 X=nx*delx
 Y=ny*dely
-
 [XX,YY]=np.meshgrid(X,Y)
+##Set up Impulse Function##
+k=2*pi/lambda0 ##wave vector
+###################################################Normalization########################################################
+averagedframei=0##this is only for normallization--whats appropriate for normalization?
+averagedframef=2005
+stackss=np.float32(np.zeros((1024,1280)))
+for f in range(averagedframei,averagedframef+1):
+    f2="{:04d}".format(f)
+    if os.path.exists(placeread.format((f2)))==False:
+        continue##if file dosnt exit 
+        else:
+            read_path=placeread.format((f2))
+            h = (tifffile.imread(read_path))
+            h1 = np.array(h).astype(np.float32)
+            stackss +=h1    
+averagestack=stackss/(len(range(averagedframei,averagedframef+1)))
+h1=h1/averagestack
+###################################################Reconstruction##########################################################
+## Convolution Steps
+H=np.fft.fft2(h1) ##fast 2d fourier trnasform of hologram
+for d in dp: 
+    num = np.exp(-i*2*pi/lambda0*np.sqrt((d**2+XX**2+YY**2))) ##numerator of impulse function
+    den = np.sqrt(d**2+XX**2+YY**2) ##denominator of impulse function
+    g=i/lambda0*num/den# impulse response of propagation
+    G=np.fft.fft2(g) ##fast 2d fourier trnasform of impusle function
+    Rec_image=np.fft.fftshift(np.fft.ifft2(G*H)) ##obtaining reconstructed field from inverse fourier transform product 
+    amp_rec_image = np.abs(Rec_image) ##the real component is the intensity 
 
-
-Gy = 10
-k=2*pi/lambda0
-
-imageslices=(maxd-mind)/steps
-minproj=np.zeros([minN,minN,imageslices])
-
-metricarray=[]
-dp = np.arange(mind, maxd, steps)
-H=np.fft.fft2(h1)
-
-indd=np.arange(1,minN,1)
-
-
-threeD=np.empty((imageslices,minN,minN))
-for d in dp:
-    ind=(d-mind)/steps
-    num = np.exp(-i*2*pi/lambda0*np.sqrt((d**2+XX**2+YY**2)))
-    den = np.sqrt(d**2+XX**2+YY**2)
-    g=i/lambda0*num/den#g is the impulse response of propagation
-    G=np.fft.fft2(g)
-    Rec_image=np.fft.fftshift(np.fft.ifft2(G*H))
-    amp_rec_image = np.abs(Rec_image)
-    threeD[ind,:,:]=amp_rec_image
     
